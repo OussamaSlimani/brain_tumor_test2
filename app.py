3b42fcfd3ae4e5e04b901a47d4e3ca73607d41a4
Oussama Slimani <slimani.oussama@enis.tn>
2024-03-02 18:30:35 +0100

    final version 1.0

--- app.py
+++ app.py
@@ -1,14 +1,79 @@
-
-
 from flask import Flask, render_template, request
+from PIL import Image
 import os
 import torch
-import numpy as np
-from PIL import Image
 from torchvision import transforms
-from torchvision.models.mobilenetv3 import mobilenet_v3_large as Model
+import matplotlib.pyplot as plt
+from pytorch_grad_cam import GradCAMPlusPlus as CAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from torchvision.models.resnet import resnet18 as ResNetModel
+from torchvision.models.resnet import BasicBlock
+from torchvision.models.mobilenetv3 import mobilenet_v3_large as MobileNetV3Model
+from werkzeug.utils import secure_filename
+import imghdr
 
 app = Flask(__name__)
+plt.switch_backend('Agg')
+
+IMAGE_SIZE = 224
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def generate_and_save_heatmap(image_path, model, combined_figure_path):
+    # Load the image
+    image = Image.open(image_path).convert('RGB')
+
+    # Preprocess the image
+    transform = transforms.Compose([
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.ToTensor(),
+    ])
+    input_image = transform(image).unsqueeze(0)
+
+    # Move the model and input image to the correct device
+    model.to(DEVICE)
+    input_image = input_image.to(DEVICE)
+
+    # Predict the class
+    output = model(input_image)
+    predicted_class = output.argmax(dim=1).item()
+
+    # Generate GradCAMPlusPlus heatmap
+    cam = CAM(model=model, target_layers=[model.layer3[-1]])
+    target = [ClassifierOutputTarget(predicted_class)]
+    grayscale_cam = cam(input_tensor=input_image, targets=target)
+
+    # Convert to RGB and overlay on the original image
+    visualization = show_cam_on_image(
+        input_image.cpu().numpy().squeeze().transpose(1, 2, 0),
+        grayscale_cam[0, :],
+        use_rgb=True
+    )
+
+    # Display the original image, prediction, and segmented image
+    plt.subplot(1, 2, 1)
+    plt.imshow(image)
+    plt.title(f'Original Image')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(visualization)
+    plt.title('Segmentation Heatmap')
+
+
+    # Remove x and y axis labels
+    plt.subplot(1, 2, 1).set_xticks([])
+    plt.subplot(1, 2, 1).set_yticks([])
+    plt.subplot(1, 2, 2).set_xticks([])
+    plt.subplot(1, 2, 2).set_yticks([])
+
+    # Save the combined figure
+    plt.savefig(combined_figure_path, bbox_inches='tight')
+
+# Usage example for segmentation
+model_seg = ResNetModel()
+model_seg.fc = torch.nn.Linear(in_features=model_seg.fc.in_features, out_features=4)
+model_seg.load_state_dict(torch.load('best_model-2.pth', map_location=DEVICE))
+model_seg.eval()
 
 def predict_tumor_class(image_path):
     # Define the classes list
@@ -18,14 +83,14 @@ def predict_tumor_class(image_path):
     model_path = "best_model.pth"  # Assuming the PyTorch model is saved as .pth
 
     # Load the trained model
-    model = Model(weights=None)  # Initialize MobileNetV3 model
-    model.classifier[3] = torch.nn.Linear(in_features=model.classifier[3].in_features, out_features=len(classes))
+    model_det = MobileNetV3Model()
+    model_det.classifier[3] = torch.nn.Linear(in_features=model_det.classifier[3].in_features, out_features=len(classes))
 
     # Use GPU if available
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    model.to(device)
-    model.eval()
+    device_det = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_det.load_state_dict(torch.load(model_path, map_location=device_det))
+    model_det.to(device_det)
+    model_det.eval()
 
     # Load and preprocess the image
     transform_val = transforms.Compose([transforms.ToTensor()])
@@ -33,23 +98,17 @@ def predict_tumor_class(image_path):
     try:
         img = Image.open(image_path).convert('RGB')
     except Exception as e:
-        print("Error opening the image:", e)
         return "Error processing the image. Please make sure it's a valid image file."
 
     try:
-        img = transform_val(img).unsqueeze(0).to(device)
+        img = transform_val(img).unsqueeze(0).to(device_det)
     except Exception as e:
-        print("Error processing the image:", e)
         return "Error processing the image. Please try again."
-
-    # Add debug statements
-    print("Image path:", image_path)
-    print("Image shape:", img.shape)
 
     # Make predictions
     with torch.no_grad():
-        output = model(img)
-        probabilities = torch.nn.functional.softmax(output, dim=-1)
+        output_det = model_det(img)
+        probabilities = torch.nn.functional.softmax(output_det, dim=-1)
         predicted_class_index = torch.argmax(probabilities, dim=-1).item()
 
     # Check if the prediction is empty
@@ -60,34 +119,46 @@ def predict_tumor_class(image_path):
     class_label = classes[predicted_class_index]
     return class_label
 
-# Configuring the static folder
-app.static_folder = 'static'
+
+def convert_file_path(original_path):
+    # Replace backslashes with forward slashes
+    converted_path = original_path.replace("\\", "/")
+    
+    # Remove the 'static/' prefix if present
+    if converted_path.startswith("static/"):
+        converted_path = converted_path[len("static/"):]
+
+    return converted_path
 
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         # Get the uploaded file
         imagefile = request.files['imagefile']
-        image_path = os.path.join("images", imagefile.filename)
+        image_path = os.path.join("static","upload",secure_filename(imagefile.filename))
 
         try:
             imagefile.save(image_path)
+            if imghdr.what(image_path) not in ['jpeg', 'png', 'gif', 'bmp']:
+                raise ValueError("Invalid image format")
         except Exception as e:
-            print("Error saving the image:", e)
-            return render_template('index.html', error="Error saving the image. Please try again.")
+            print("Error saving or validating the image:", e)
+            return render_template('index.html', error="Error saving or validating the image. Please try again."), 400
 
         try:
             predicted_class = predict_tumor_class(image_path)
         except Exception as e:
             print("Error predicting tumor class:", e)
-            return render_template('index.html', error="Error predicting tumor class. Please try again.")
+            return render_template('index.html', error="Error predicting tumor class. Please try again."), 400
 
-        # Add debug statement
-        print("Predicted class:", predicted_class)
+        # Generate heatmap
+        generate_and_save_heatmap(image_path, model_seg, image_path)
+        converted_path = convert_file_path(image_path)
+        return render_template('result.html', prediction=predicted_class, image_path=converted_path)
 
-        return render_template('result.html', prediction=predicted_class)
 
     return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
+
